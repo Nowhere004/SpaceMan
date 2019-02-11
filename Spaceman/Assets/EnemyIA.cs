91c74c0bbed14371ad4f0f060a7ba977b7355629
Nowhere004 <arcanakgul@hotmail.com>
2019-02-12 00:00:26 +0300

    All_Game

---
+++ Spaceman/Assets/EnemyIA.cs
@@ -0,0 +1,133 @@
+﻿using Pathfinding;
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Rigidbody2D))]
+[RequireComponent(typeof(Seeker))]
+public class EnemyIA : MonoBehaviour {
+    //what to chase?
+    public Transform target;
+    //how many times each second we will update our path
+    public float UpdateRate = 2f;
+
+    private Seeker seeker;
+    private Rigidbody2D rb;
+
+    //The Calculated Path
+    public Path path;
+    //The IA's speed per second
+    public float speed = 300f;
+    public ForceMode2D fMode;
+
+    [HideInInspector]
+    public bool pathIsEnded=false;
+    //Max Distance from the IA to a waypoint for it to continue to the next waypoint
+    public float nextWayPointDistance = 3f;
+
+    //The waypoint we are currently moving towards
+    private int currentWaypoint = 0;
+
+    private bool searchingForPlayer = false;
+
+
+
+
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+        if (target==null)
+        {
+            if (!searchingForPlayer)
+            {
+                searchingForPlayer = true;
+                StartCoroutine(SearchForPlayer());
+            }
+            return;
+        }
+        //
+        seeker.StartPath(transform.position,target.position,OnPathComplate);
+        StartCoroutine(UpdatePath());
+    }
+    IEnumerator SearchForPlayer()
+    {
+        GameObject sResult= GameObject.FindGameObjectWithTag("Player");
+        if (sResult == null) { 
+            yield return new WaitForSeconds(0.5f);
+            StartCoroutine(SearchForPlayer());
+        }
+        else
+        {
+            target = sResult.transform;
+            searchingForPlayer = false;
+            yield return false;
+            StartCoroutine(UpdatePath());
+        }
+    }
+
+    public void OnPathComplate(Path p)
+    {
+        Debug.Log("We Got a Path.Did it Have an Error"+p.error);
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+
+        }
+    }
+    IEnumerator UpdatePath()
+    {
+        if (target == null)
+        {
+            if (!searchingForPlayer)
+            {
+                searchingForPlayer = true;
+                StartCoroutine(SearchForPlayer());
+            }
+            yield return false;
+        }else
+        seeker.StartPath(transform.position, target.position, OnPathComplate);
+        yield return new WaitForSeconds(1f/ UpdateRate) ;
+        StartCoroutine(UpdatePath());
+
+    }
+
+    void FixedUpdate()
+    {
+        if (target == null)
+        {
+            //TODO: Insert a player search here
+            return;
+        }
+
+        //TODO: Always Look At The Player
+        if (path==null)
+            return;
+        if (currentWaypoint>=path.vectorPath.Count)
+        {
+            if (pathIsEnded)
+                return;
+            Debug.Log("End of Path Reached");
+            pathIsEnded = true;
+            return;
+
+        }
+        pathIsEnded = false;
+        //Direction to the next waypoint
+        Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
+        dir *= speed*Time.fixedDeltaTime;
+
+        //Move the IA
+        rb.AddForce(dir,fMode);
+        float dist = Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]);
+        if (dist<nextWayPointDistance)
+        {
+            currentWaypoint++;
+            return; 
+
+        }
+
+    }
+
+    
+}
---
+++ Spaceman/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour {
+
+    public static GameMaster gm;
+    
+    void Awake()
+    {
+        if (gm == null)
+        {
+            gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        }
+    }
+
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+    public float spawnDelay = 2f;
+    public Transform spawnPrefab;
+    public AudioClip[] Sounds;
+    public CameraShake cameraShake;
+
+  void Start()
+    {
+        if (cameraShake==null)
+        {
+            Debug.LogError("No camera Shake referenced in Gamemaster");
+        }
+    }
+   
+
+    public IEnumerator RespawnPlayer()
+    {
+        //Debug.Log("TODO: Add waiting for spawn sound");
+        GetComponent<AudioSource>().PlayOneShot(Sounds[0]);
+        yield return new WaitForSeconds(spawnDelay);
+
+        Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+        GameObject clone= (Instantiate(spawnPrefab,spawnPoint.position,spawnPoint.rotation)).gameObject;
+        Destroy(clone, 3f);
+        //Debug.Log("TODO: Add Spawn Particles");
+    }
+
+    public static void KillPlayer(Player player)
+    {
+        Destroy(player.gameObject);
+        gm.StartCoroutine(gm.RespawnPlayer());
+    }
+    public static void KillEnemy(Enemy enemy)
+    {
+        
+        gm._KillEnemy(enemy);
+    }
+    public void _KillEnemy(Enemy _enemy)
+    {
+        GameObject clone=(Instantiate(_enemy.deathParticles,_enemy.transform.position,Quaternion.identity)).gameObject;
+        Destroy(clone);
+        cameraShake.Shake(_enemy.shakeAmt,_enemy.shakeLength);
+        Destroy(_enemy.gameObject);
+
+    }
+}
